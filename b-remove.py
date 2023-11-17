c5213eab1d8c9eeae19891e1ce465374e88a6eb7
tanuki-create <136745803+tanuki-create@users.noreply.github.com>
2023-11-17 03:56:06 -0800

    Update b-remove.py

--- b-remove.py
+++ b-remove.py
@@ -1,98 +1,116 @@
-from rembg import remove
-from PIL import Image
 import os
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import ttk
 import threading
+import tkinter as tk
+from tkinter import filedialog, ttk, messagebox
+from PIL import Image
+from rembg import remove
 
-root = tk.Tk()
+class BatchRemovalApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("640x360")
+        self.root.title("Batch Background Removal Tool")
 
-def set_default_in():
-    with open("default_in.txt", "w") as file:
-        file.write(named_directory_in.get())
+        self.setup_style()
 
-def set_default_out():
-    with open("default_out.txt", "w") as file:
-        file.write(named_directory_out.get())
+        self.named_directory_in = tk.StringVar(value=self.load_default("default_in.txt"))
+        self.named_directory_out = tk.StringVar(value=self.load_default("default_out.txt"))
 
-def get_path_in():
-    named_directory_in.set(filedialog.askdirectory())
+        self.setup_widgets()
 
-def get_path_out():
-    named_directory_out.set(filedialog.askdirectory())
+    def setup_style(self):
+        style = ttk.Style(self.root)
+        style.theme_use('clam')
 
-def run_batch_removal_tool():
-    pic_list = os.listdir(named_directory_in.get())
-    save_number = 0
+        # ボタンスタイル
+        style.configure('TButton', font=('Helvetica', 10), background='#89CFF0')
 
-    # Loop over all Images
-    for pic in pic_list:
-        save_number += 1
-        input_path = os.path.join(named_directory_in.get(), pic)
-        
-        # Store path of the output image in the variable output_path
-        output_path = os.path.join(named_directory_out.get(), f'no_bg{save_number}.png')
+        # ラベルスタイル
+        style.configure('TLabel', font=('Helvetica', 12), background='#89CFF0')
 
-        # Check if image exists
-        if os.path.exists(output_path):
-            continue
-        
-        # Processing the image
-        input_image = Image.open(input_path)
+        # プログレスバースタイル
+        style.configure('Horizontal.TProgressbar', background='#89CFF0', troughcolor='white', bordercolor='gray', lightcolor='#89CFF0', darkcolor='#89CFF0')
 
-        # Removing the background from the given Image
-        output = remove(input_image)
+        # GUI背景色
+        self.root.configure(background='#89CFF0')
 
-        # Saving the image in the given path
-        output.save(output_path)
+    def setup_widgets(self):
+        ttk.Label(self.root, text="Batch Background Removal Tool", font=("Helvetica", 16, 'bold')).grid(row=0, column=1, pady=10)
 
-# Load default paths if the files exist
-if os.path.exists("default_in.txt"):
-    with open("default_in.txt", "r") as file:
-        saved_default_in = file.read()
-else:
-    saved_default_in = ""
+        # Input path selection
+        ttk.Button(self.root, text="Choose Input Path", command=self.get_path_in).grid(row=1, column=0, sticky="EW", padx=10, pady=10)
+        ttk.Label(self.root, textvariable=self.named_directory_in, relief="sunken").grid(row=1, column=1, sticky="EW", padx=10)
+        ttk.Button(self.root, text="Set as Default", command=lambda: self.set_default("default_in.txt", self.named_directory_in)).grid(row=1, column=2, padx=10)
 
-if os.path.exists("default_out.txt"):
-    with open("default_out.txt", "r") as file:
-        saved_default_out = file.read()
-else:
-    saved_default_out = ""
+        # Output path selection
+        ttk.Button(self.root, text="Choose Output Path", command=self.get_path_out).grid(row=2, column=0, sticky="EW", padx=10, pady=10)
+        ttk.Label(self.root, textvariable=self.named_directory_out, relief="sunken").grid(row=2, column=1, sticky="EW", padx=10)
+        ttk.Button(self.root, text="Set as Default", command=lambda: self.set_default("default_out.txt", self.named_directory_out)).grid(row=2, column=2, padx=10)
 
-named_directory_in = tk.StringVar(root, saved_default_in)
-named_directory_out = tk.StringVar(root, saved_default_out)
+        # Progress bar
+        self.progress_bar = ttk.Progressbar(self.root, orient='horizontal', length=300, mode='determinate')
+        self.progress_bar.grid(row=3, column=1, pady=10)
 
-root.geometry("640x250")
-root.title("Batch Background Removal Tool")
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=1)
+        # 完了ラベルの追加
+        self.completion_label = ttk.Label(self.root, text="", font=("Helvetica", 10))
+        self.completion_label.grid(row=4, column=1, pady=10)
 
-# GUI Title
-ttk.Label(root, text="Uncha - Batch Background Removal Tool", padding=(30, 30)).grid(row=0, column=1)
+        # Start and Quit buttons
+        ttk.Button(self.root, text="Start Background Removal Tool", command=self.start_thread).grid(row=4, column=1, pady=10)
+        ttk.Button(self.root, text="Quit", command=self.root.destroy).grid(row=5, column=1, pady=10)
 
-# File In Button and Info Label
-choose_path_in_button = ttk.Button(root, text="Choose Input Path", command=get_path_in).grid(row=1, column=0, sticky="EW")
-ttk.Label(root, textvariable=named_directory_in, relief="sunken").grid(row=1, column=1, sticky="EW")
-set_default_out_button = ttk.Button(root, text="Set as Default", command=set_default_in).grid(row=1, column=2)
+    def load_default(self, file_name):
+        if os.path.exists(file_name):
+            with open(file_name, "r") as file:
+                return file.read()
+        return ""
 
-# File Out Button and Info Label
-choose_path_out_button = ttk.Button(root, text="Choose Output Path", command=get_path_out).grid(row=2, column=0, sticky="EW")
-ttk.Label(root, textvariable=named_directory_out, relief="sunken").grid(row=2, column=1, sticky="EW")
-set_default_out_button = ttk.Button(root, text="Set as Default", command=set_default_out).grid(row=2, column=2)
+    def set_default(self, file_name, path_variable):
+        with open(file_name, "w") as file:
+            file.write(path_variable.get())
 
-# Run Tool Button
-def start_thread():
-    threading.Thread(target=run_batch_removal_tool).start()
+    def get_path_in(self):
+        self.named_directory_in.set(filedialog.askdirectory())
 
-run_batch_removal_tool_button = ttk.Button(
-    root,
-    text="Start Background Removal Tool",
-    command=start_thread
-).grid(row=3, column=1, pady=10)
+    def get_path_out(self):
+        self.named_directory_out.set(filedialog.askdirectory())
 
-# Quit GUI and process Button
-quit_button = ttk.Button(root, text="Quit", command=root.destroy).grid(row=4, column=1)
+    def start_thread(self):
+        # スレッドの開始前にGUIをリセット
+        self.completion_label['text'] = ""  
+        self.progress_bar['value'] = 0
+        threading.Thread(target=self.run_batch_removal_tool, daemon=True).start()
 
-root.mainloop()
+    def run_batch_removal_tool(self):
+        pic_list = os.listdir(self.named_directory_in.get())
+        total = len(pic_list)
+        self.progress_bar['maximum'] = total
+        save_number = 0
+
+        for pic in pic_list:
+            save_number += 1
+            input_path = os.path.join(self.named_directory_in.get(), pic)
+            output_path = os.path.join(self.named_directory_out.get(), f'no_bg{save_number}.png')
+
+            if os.path.exists(output_path):
+                continue
+
+            try:
+                input_image = Image.open(input_path)
+                output = remove(input_image)
+                output.save(output_path)
+            except Exception as e:
+                self.root.after(0, lambda: messagebox.showerror("Error", f"Failed to process {pic}: {e}"))
+                return
+
+            # GUIの更新（プログレスバーの進捗）
+            self.root.after(0, lambda value=save_number: self.update_progress_bar(value))
+
+        # 処理完了後のGUI更新
+        self.root.after(0, self.update_completion_label)
+
+    def update_progress_bar(self, value):
+        self.progress_bar['value'] = value
+
+    def update_completion_label(self):
+        self.completion_label['text'] = "Complete"
